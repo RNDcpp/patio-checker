1a068e5f0f98e6d7f2cab3b9d0b5f58a77d2ab75
RNDcpp <rnd@pixiv.com>
2020-12-06 02:07:57 +0900

    chore: 100件まで取得

--- lib/twilog_parser/twilog_parser.rb
+++ lib/twilog_parser/twilog_parser.rb
@@ -4,13 +4,14 @@ require 'csv'
 
 module TwilogParser
   class Parser
-    def get(screen_name, limit: 1, sleep_sec: 5)
+    def get(screen_name, limit: 100, sleep_sec: 2)
       total_tweets = []
       prev_id = nil
       limit.times do |i|
         html = URI.open("http://twilog.org/#{screen_name}/nort-#{i+1}",'User-Agent'=>'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)') do |f|
           f.read
         end
+        puts "#{screen_name} #{i+1}"
         doc = Nokogiri::HTML.parse(html,nil,'UTF-8')
         # title01
         id = doc.xpath('.//*[@class = "tl-tweet"]').first.attributes['id'].value

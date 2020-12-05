beaa9c152d1016dfff0c24044e921b66f07572cf
RNDcpp <rnd@pixiv.com>
2020-12-06 02:33:16 +0900

    feat: null safe and screen_names

--- lib/twilog_parser/twilog_parser.rb
+++ lib/twilog_parser/twilog_parser.rb
@@ -8,26 +8,35 @@ module TwilogParser
       total_tweets = []
       prev_id = nil
       limit.times do |i|
-        html = URI.open("http://twilog.org/#{screen_name}/nort-#{i+1}",'User-Agent'=>'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)') do |f|
-          f.read
+        sleep(sleep_sec)
+        begin
+          html = URI.open("http://twilog.org/#{screen_name}/nort-#{i+1}",'User-Agent'=>'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)') do |f|
+            f.read
+          end
+        rescue => e
+          puts 'Error Occured'
+          puts e.message
+          sleep(20)
+          continue
         end
         puts "#{screen_name} #{i+1}"
         doc = Nokogiri::HTML.parse(html,nil,'UTF-8')
         # title01
-        id = doc.xpath('.//*[@class = "tl-tweet"]').first.attributes['id'].value
+        id = doc.xpath('.//*[@class = "tl-tweet"]')&.first&.attributes['id']&.value
+        break unless id
         break if prev_id == id
         prev_id = id
         dates = doc.xpath('//*[@class = "title01"]')
         tweets = dates.zip(doc.xpath('//*[@class = "tl-tweets"]')).map do |date_dom, tl|
-          date = date_dom.xpath('.//a').first.text&.gsub(/(年|月)/,'/')&.gsub(/日\(.\)/,'')
+          date = date_dom.xpath('.//a')&.first&.text&.gsub(/(年|月)/,'/')&.gsub(/日\(.\)/,'')
           tl.xpath('.//*[@class = "tl-tweet"]').map do |t|
             text = t.xpath('.//*[@class = "tl-text"]').first&.text
             hms = t.xpath('.//*[@class = "tl-posted"]').first&.text&.gsub('posted at ','')
+            break unless text && hms
             date_time = date + ' ' + hms + ' +0900'
             Tweet.new(screen_name: screen_name, text: text, posted_at: Time.parse(date_time))
           end
         end.flatten
-        sleep(sleep_sec)
         total_tweets += tweets
       end
       TweetsCollection.new(total_tweets)

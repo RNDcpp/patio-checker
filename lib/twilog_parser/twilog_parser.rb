0c7e9534dbe12fe5cdc0e8348ece7d457d2a408e
RNDcpp <rnd@pixiv.com>
2020-12-06 00:53:17 +0900

    feat: not load duplicated tweets

--- lib/twilog_parser/twilog_parser.rb
+++ lib/twilog_parser/twilog_parser.rb
@@ -3,13 +3,18 @@ require 'nokogiri'
 
 module TwilogParser
   class Parser
-    def get(screen_name, limit: 1, sleep_sec: 5)
+    def get(screen_name, limit: 5, sleep_sec: 5)
+      total_tweets = []
+      prev_id = nil
       limit.times do |i|
         html = URI.open("http://twilog.org/#{screen_name}/nort-#{i+1}",'User-Agent'=>'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)') do |f|
           f.read
         end
         doc = Nokogiri::HTML.parse(html,nil,'UTF-8')
         # title01
+        id = doc.xpath('.//*[@class = "tl-tweet"]').first.attributes['id'].value
+        break if prev_id == id
+        prev_id = id
         dates = doc.xpath('//*[@class = "title01"]')
         tweets = dates.zip(doc.xpath('//*[@class = "tl-tweets"]')).map do |date_dom, tl|
           date = date_dom.xpath('.//a').first.text&.gsub(/(年|月)/,'/')&.gsub(/日\(.\)/,'')
@@ -22,8 +27,9 @@ module TwilogParser
         end.flatten
         binding.irb
         sleep(sleep_sec)
+        total_tweets += tweets
       end
-      tweets
+      total_tweets
     end
   end
 

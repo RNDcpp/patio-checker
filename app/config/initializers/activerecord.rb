bd58871a63b6bcce7174aea58052a9ea82ad50d9
RNDcpp <yonbanki@gmail.com>
2020-12-05 13:41:57 +0900

    fix: active_record initializer config path

--- app/config/initializers/activerecord.rb
+++ app/config/initializers/activerecord.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 
-config = Config::load_yaml('config/database.yml')
+config = Config::load_yaml('database.yml')
 env = ENV.fetch('APP_ENV', 'development')
 ActiveRecord::Base.establish_connection(config[env])
 ActiveRecord::Base.time_zone_aware_attributes = true

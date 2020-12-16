1b05a0589d62f63f5f8c3c980712cb461594f9dd
RNDcpp <yonbanki@gmail.com>
2020-12-17 01:19:01 +0900

    feat: tweetボタン

---
+++ app/frontend/ts/components/TweetButton.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+interface tweetButtonProps {
+  doc: string,
+  point: number
+}
+
+declare global {
+  interface Window {
+    twttr?: any;
+  }
+}
+
+const TBtn = styled.a`
+background-color: #1DA1F2;
+color: #fff;
+padding: 0.5rem;
+text-decoration: none;
+display: inline-block;
+margin-right: 3rem;
+border: solid;
+border-left-color:  #c9eaff;
+border-top-color: #c9eaff;
+border-bottom-color: #007ccf;
+border-right-color: #0067a7;
+border-width: 1px;
+`
+
+const SharePane = styled.div`
+display: flex;
+justify-content: flex-end;
+padding: 0.5rem;
+border-top: solid;
+border-width: 1px;
+border-color: rgba(255, 255, 255, 0.5);
+`
+
+export const TweetButton: React.FC<tweetButtonProps> = (props) => {
+  const wrappedText = (doc:string, point:number) => `
+  『${doc}』のぱちお度は${point}%でした。
+  #ぱちお診断
+  `
+  const url = `https://twitter.com/intent/tweet?url=${location.href}&text=${encodeURI(wrappedText(props.doc, props.point))}`
+  return(
+    <SharePane>
+      <TBtn
+        href={url}
+        className="twitter-share-button"
+        target="_blank"
+      >Tweet</TBtn>
+    </SharePane>
+  )
+}
